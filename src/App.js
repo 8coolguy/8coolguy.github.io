f645f7e22d9025be74ec24418762db3df12a000f
Arnav Choudhury <66573183+8coolguy@users.noreply.github.com>
2024-06-17 23:34:49 -0700

    back and forth

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import './App.css';
+import {projects, experiences} from './info.js';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
 import Card from 'react-bootstrap/Card';
 import image from "./backshot.jpg";
-import action from "./action.jpeg";
-import notFound from "./notfound.png";
-import resume from "./resume.pdf";
 import strava from "./strava.svg";
 import linkedin from "./linkedin.svg";
 import github from "./github.svg";
 import instagram from "./instagram.svg";
 import headshot from "./headshot.png";
-import {BrowserRouter,Link,Route,Routes} from "react-router-dom";
+import resume from "./resume.pdf";
+import {BrowserRouter,Route,Routes} from "react-router-dom";
 
 
 
@@ -30,6 +30,8 @@ export default function App() {
 }
 
 function Home() {
+  console.log(experiences);
+  console.log(projects);
   return(
     <div className="App">
       <Navbar className="navbar">
@@ -83,7 +85,7 @@ function Home() {
               <Nav.Link href="#projects">Projects</Nav.Link>
               <Nav.Link href="#other" disabled>Other</Nav.Link>
               
-              <Nav.Link href="https://drive.google.com/file/d/1sjYX7RhOaZOO09FIlZL8K5ianLdb5Cpy/view?usp=sharing">Resume</Nav.Link>
+              <Nav.Link href="/resume">Resume</Nav.Link>
             </Nav>
         </Container>
       </Navbar>
@@ -91,155 +93,57 @@ function Home() {
       
       
       <Row style={{backgroundImage:`url(${image})`}}  id="intro" >
-        <Container className="section">
-          <Row className="justify-content-md-center" md="auto">
-            <Col className="m-auto">
-              <img className="headshot" src={headshot}></img>
-            </Col>
-            <Col>
-              
-              <Card>
-                  <Card.Body>
-                <p>hii. My name is
-                <h1 > Arnav Choudhury</h1>
-                I am currently an undergrad Computer Science and Engineering student at Santa Clara University and I spend time building personal projects to improve my skills Right now I am focusing on Backend Development and Fullstack development, but I also have interests in Game Development and Machine Learning. Contact me at achoudhury2@scu.edu
-                </p>
-                </Card.Body>
-              </Card>
-              
-            </Col>
-          </Row> 
-        </Container>
-      </Row>
-      
-    
-      
-      
-    
-      <Row id="projects" >
-        <Container className="section">
-          <h1>Projects</h1>
-          <Row xs={1} md={2} className="g-4">
-          <Card className='project' style={{ width: '20rem' }}>
-            <Card.Img variant="top" src="https://i.imgur.com/wjhMVRO.png" />
-            <Card.Body>
-              <Card.Title>Chill Spaces</Card.Title>
-              <Card.Subtitle className="mb-2 text-muted">November 2022</Card.Subtitle>
-              <Card.Text>
-                Website that helps you find the quietest parts of a city. Built with React, Firebase, and Flask.
-              </Card.Text>
-              <Card.Link href="https://devpost.com/software/chillspaces">Devpost</Card.Link>
-            </Card.Body>
-          </Card>
+        <Row className="justify-content-md-center" md="auto">
           
-          <Card className='project' style={{ width: '20rem' }}>
-            <Card.Img variant="top" src="https://i.imgur.com/czxFKmU.jpg" />
-            <Card.Body>
-              <Card.Title>Carbon Footprint Tracker</Card.Title>
-              <Card.Subtitle className="mb-2 text-muted">September 2022</Card.Subtitle>
-              <Card.Text>
-                Website that helps you track and visualize your carbon footprint by tracking six categories of usage. Built with React and Firebase.
-              </Card.Text>
-              <Card.Link href="#">Github</Card.Link>
-            </Card.Body>
-          </Card>
-
-          <Card className='project' style={{ width: '20rem' }}>
-            <Card.Img variant="top" src="https://imgur.com/VFgIFUX.png" />
-            <Card.Body>
-              <Card.Title>Blog Graph</Card.Title>
-              <Card.Subtitle className="mb-2 text-muted">July 2022</Card.Subtitle>
-              <Card.Text>
-                Website that helps you blog with a graph visualization. Built with React, Mongo and AWS.
-              </Card.Text>
-              <Card.Link href="https://github.com/8coolguy/BlogGraph">Github</Card.Link>
-            </Card.Body>
-          </Card>
-          <Card className='project' style={{ width: '20rem' }}>
-            <Card.Img variant="top" src="https://i.imgur.com/4DwHWaA.png" />
-            <Card.Body>
-              <Card.Title>California Predictive Fire Model</Card.Title>
-              <Card.Subtitle className="mb-2 text-muted">December 2021</Card.Subtitle>
-              <Card.Text>
-              With CAL Fire data of the past 10 years of wildfires, weather data, and local county data, I am trying to build a predictive fire model that tries to predict the size of a wild fire based on given variables.
-              </Card.Text>
-              <Card.Link href="https://github.com/8coolguy/cali-fire-analysis">Github</Card.Link>
-            </Card.Body>
-          </Card>
-          <Card className='project' style={{ width: '20rem' }}>
-            <Card.Img variant="top" src="https://i.imgur.com/yG7f32h.png" />
-            <Card.Body>
-              <Card.Title>Paper Prisons Crime Classifiers Model</Card.Title>
-              <Card.Subtitle className="mb-2 text-muted">October 2021</Card.Subtitle>
-              <Card.Text>
-                Built a language processor that would be able to classify whether a criminal charge can be expunged or not. Able to classify 50% of the given datasets as expungeable or non-expungeable crime.
-              </Card.Text>
-              <Card.Link href="https://github.com/8coolguy/paper_prisons_crime_classifiers">Github</Card.Link>
-            </Card.Body>
-          </Card>
-
-          <Card style={{ width: '20rem' }}>
-            <Card.Img variant="top" src="https://i.imgur.com/7WkeEGY.png" />
-            <Card.Body>
-              <Card.Title>Rabbit Ecosystem Simulation</Card.Title>
-              <Card.Subtitle className="mb-2 text-muted">July 2021</Card.Subtitle>
-              <Card.Text>
-              Programmed a simulation of a rabbit ecosystem where I tracked how it reacted to external variables like predators, water, land features. Developed a Q learning reinforcement model for each rabbit to make decisions based on their surroundings. Utilized the concepts of polymorphism and inheritance to effectively create simulated animals.
-              </Card.Text>
-              <Card.Link href="https://github.com/8coolguy/eco-system-sim">Github</Card.Link>
-            </Card.Body>
-          </Card>
-
-          <Card className='project' style={{ width: '20rem' }}>
-            <Card.Img variant="top" src="https://i.imgur.com/6vtGrmj.gif" />
-            <Card.Body>
-              <Card.Title>Proceduraly Generated Solar System</Card.Title>
-              <Card.Subtitle className="mb-2 text-muted">June 2021</Card.Subtitle>
-              <Card.Text>
-              Created a simulation using Unity that can proceduraly generate a solar system with unique planets and stars. Implemented different methods of coherent noise to planets to generate unique terrain on planets.
-              </Card.Text>
-              <Card.Link href="https://github.com/8coolguy/pcg-solar-system/">Github</Card.Link>
-            </Card.Body>
-          </Card>
-          </Row>
-    
-          
-
-            
-            
-            
+          <Col md = {6}>
+            <Container className="left-container">
+              <Row>
+                <Col className="m-auto">
+                  <img className="headshot" src={headshot}></img>
                 
-
+                  
+                  <Card>
+                      <Card.Body>
+                    <p>hii. My name is
+                    <h1 > Arnav Choudhury</h1>
+                    I am currently an undergrad Computer Science and Engineering student at Santa Clara University and I spend time building personal projects to improve my skills Right now I am focusing on Backend Development and Fullstack development, but I also have interests in Game Development and Machine Learning. Contact me at achoudhury2@scu.edu
+                    </p>
+                    </Card.Body>
+                  </Card>
+                  
+                </Col>
+              </Row>
+            </Container>
+          </Col>
           
           
-        
-          
-          
-          
-          
-              
-                
-        </Container>
-      </Row>
-      <Row id="triathlon" >
-        <Container className="section">
-        <h1>Triathlon</h1>
-        <Row>
-
-        <Col xs={6} md={4}>
-        <img className="action" src={action}></img>
-        </Col>
-        <Col xs={6} md={4}>
-        <Card>
-          <Card.Body>I am currently the vice president of the Santa Clara Triathlon club. We are trying to grow and collabrative and competitve environment for new and experienced members to thrive. I also work hard to try to compete at a high level </Card.Body>
-        </Card>
-          
-        </Col>
-        <Col xs={6} md={4}><iframe height='454' width='300' frameborder='0' allowtransparency='true' scrolling='no' src='https://www.strava.com/athletes/33234384/latest-rides/b83afe7a36c7074e111ae54598b2463d5166cb3d'></iframe></Col>
-        
-        </Row>  
-        </Container>
-        
+          <Col md ={6}>
+            <Container className="right-container">
+              <div id="projects">
+                {/* <h1>Projects</h1> */}
+                {projects.slice(0,10).map(project => 
+                  <Card mb ={3} className="project">
+                    <Row g={0} className = "flex" noGutters>
+                    <Col md = {4} >
+                      <Image fluid className="img-fluid" src={project.image} />
+                    </Col>
+                    <Col md = {8}>
+                    <Card.Body>
+                      <Card.Title>{project.name}</Card.Title>
+                      <Card.Subtitle className="text-muted">{project.date}</Card.Subtitle>
+                      <Card.Text>
+                        {project.description}
+                      </Card.Text>
+                      <Card.Link href ={project.links.Devpost}> Devpost </Card.Link>
+                    </Card.Body>
+                    </Col>
+                    </Row>
+                  </Card>
+                )}     
+              </div>
+            </Container>
+          </Col>
+        </Row>
       </Row>
       
   
---
+++ src/info.js
@@ -0,0 +1,91 @@
+export let projects = [
+    {
+        name: "Chill Spaces",
+        image: "https://i.imgur.com/wjhMVRO.png",
+        description:"Website that helps you find the quietest parts of a city. Built with React, Firebase, and Flask.",
+        date : "November 2022",
+        links: {
+            Github: "",
+            Devpost: "https://devpost.com/software/chillspaces",
+            Live:"https://shur1m.github.io/InrixHack2022/",
+        }
+    },
+    {
+        name: "Carbon Footprint Tracker",
+        image: "https://i.imgur.com/czxFKmU.jpg",
+        description:"Website that helps you track and visualize your carbon footprint by tracking six categories of usage. Built with React and Firebase.",
+        date : "July 2022",
+        links: {
+            Github: "google.com",
+            Devpost: "google.com",
+            Live:"jsdfljsflkj",
+        }
+    },
+    {
+        name: "Blog Graph",
+        image: "https://imgur.com/VFgIFUX.png",
+        description:"Website that helps you blog with a graph visualization. Built with React, Mongo and AWS.",
+        date : "June 2022",
+        links: {
+            Github: "https://github.com/8coolguy/BlogGraph",
+            Devpost: "",
+            Live:"",
+        }
+    },
+    {
+        name: "California Predictive Fire Model",
+        image: "https://i.imgur.com/wjhMVRO.png",
+        description:"With CAL Fire data of the past 10 years of wildfires, weather data, and local county data, I am trying to build a predictive fire model that tries to predict the size of a wild fire based on given variables.",
+        date : "December 2021",
+        links: {
+            Github: "https://github.com/8coolguy/cali-fire-analysis",
+            Devpost: "",
+            Live:"",
+        }
+    },
+    {
+        name: "Paper Prisons Crime Classifiers Model",
+        image: "https://i.imgur.com/yG7f32h.png",
+        description:"Built a language processor that would be able to classify whether a criminal charge can be expunged or not. Able to classify 50% of the given datasets as expungeable or non-expungeable crime.",
+        date : "October 2021",
+        links: {
+            Github: "https://github.com/8coolguy/cali-fire-analysis",
+            Devpost: "",
+            Live:"",
+        }
+    },
+    {
+        name: "Rabbit Ecosystem Simulation",
+        image: "https://i.imgur.com/7WkeEGY.png",
+        description:"Programmed a simulation of a rabbit ecosystem where I tracked how it reacted to external variables like predators, water, land features. Developed a Q learning reinforcement model for each rabbit to make decisions based on their surroundings. Utilized the concepts of polymorphism and inheritance to effectively create simulated animals.",
+        date : "August 2021",
+        links: {
+            Github: "https://github.com/8coolguy/cali-fire-analysis",
+            Devpost: "",
+            Live:"",
+        }
+    },
+    {
+        name: "Proceduraly Generated Solar System",
+        image: "https://i.imgur.com/6vtGrmj.gif",
+        description:"Created a simulation using Unity that can proceduraly generate a solar system with unique planets and stars. Implemented different methods of coherent noise to planets to generate unique terrain on planets.",
+        date : "June 2021",
+        links: {
+            Github: "https://github.com/8coolguy/cali-fire-analysis",
+            Devpost: "",
+            Live:"",
+        }
+    },
+]
+
+export let experiences = [
+    {
+        title: "Lead Cool Guy",
+        date:{
+            start: "June 2020",
+            end: "March 2024"
+        },
+        description:"fwefwefjwjewf wef we fwe fw efwefwefw e wef w fw wefwef wefw  wef wfsdfksjdf sd fj sdf ksdjfhwu f ",
+
+    }
+]
