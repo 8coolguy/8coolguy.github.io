ff9ac6ee5f43d35a9db9988799bccb015dc5b6c3
Arnav Choudhury <66573183+8coolguy@users.noreply.github.com>
2024-08-09 17:12:31 -0700

    new design just ddropped lol

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {projects, experiences} from './info.js';
+import {projects, experiences, about} from './info.js';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Row from 'react-bootstrap/Row';
@@ -12,8 +12,6 @@ import github from "./github.svg";
 import instagram from "./instagram.svg";
 import headshot from "./headshot.png";
 import {BrowserRouter,Route,Routes} from "react-router-dom";
-
-
 
 export default function App() {
   return (
@@ -28,97 +26,92 @@ export default function App() {
 function Home() {
   return(
     <div className="App">      
-      <Row  id="intro" >
-        <Row className="justify-content-md-center" md="auto">
-          <Col md = {6}>
+      <Row  className="background" >
+          <Col md = {4}>
             <div className="left-container">
-              <Row>
-                <Col>
-                  <Image fluid className="headshot" src={headshot}></Image>
-                  <h1> Arnav </h1>
-                </Col>
-              </Row>
-              <Row>
-                <Container fluid>
-                  <Nav className="justify-content-end">
-                  <Nav.Link href="#intro">About</Nav.Link>
-                  <Nav.Link href='#experience'>Experience</Nav.Link>
-                  <Nav.Link href="#projects">Projects</Nav.Link>
-                  <Nav.Link href="https://blog-two-teal-35.vercel.app/" >Blog</Nav.Link>
-                  <Nav.Link href="/resume.pdf">Resume</Nav.Link>
-                </Nav>
-                </Container>
-              </Row>
-              <Row fluid class="flex items-center">
-                  <Col href="https://www.instagram.com/notarnav123">
-                    <Image
-                      src = {instagram} 
-                      width="30"
-                      height="30"
-                      className="d-inline-block align-top"
-                      alt="Instagram"
-                    />
-                  </Col>
-                  <Col href="https://www.linkedin.com/in/arnav-choudhury-scu/">
-                    <Image
-                      src = {linkedin} 
-                      width="30"
-                      height="30"
-                      className="d-inline-block align-top"
-                      alt="Linkedin"
-                    />
-                  </Col>
-                  <Col href="https://github.com/8coolguy">
-                    <Image
-                      src = {github} 
-                      width="30"
-                      height="30"
-                      className="d-inline-block align-top"
-                      alt="Github"
-                    />
-                  </Col>
-                  <Col href="https://www.strava.com/athletes/33234384">
-                    <Image
-                      src = {strava} 
-                      width="30"
-                      height="30"
-                      className="d-inline-block align-top"
-                      alt="Strava"
-                    />
-                  </Col>
-              </Row>   
+              <Image fluid className="headshot" src={headshot}></Image>
+              <h1> Arnav Choudhury</h1>
+              <div className="links">
+                <Nav.Link className="link" href="#intro">About</Nav.Link>
+                <Nav.Link className="link" href='#experience'>Experience</Nav.Link>
+                <Nav.Link className="link" href="#projects">Projects</Nav.Link>
+                <Nav.Link className="link" href="https://blog-two-teal-35.vercel.app/" >Blog</Nav.Link>
+                <Nav.Link className="link" href="/resume.pdf">Resume</Nav.Link>
+              </div>
+
+
+            <Row>
+              <Col href="https://www.instagram.com/notarnav123">
+                <Image
+                  src = {instagram} 
+                  width="30"
+                  height="30"
+                  className="d-inline-block align-top"
+                  alt="Instagram"
+                />
+              </Col>
+              <Col href="https://www.linkedin.com/in/arnav-choudhury-scu/">
+                <Image
+                  src = {linkedin} 
+                  width="30"
+                  height="30"
+                  className="d-inline-block align-top"
+                  alt="Linkedin"
+                />
+              </Col>
+              <Col href="https://github.com/8coolguy">
+                <Image
+                  src = {github} 
+                  width="30"
+                  height="30"
+                  className="d-inline-block align-top"
+                  alt="Github"
+                />
+              </Col>
+              <Col href="https://www.strava.com/athletes/33234384">
+                <Image
+                  src = {strava} 
+                  width="30"
+                  height="30"
+                  className="d-inline-block align-top"
+                  alt="Strava"
+                />
+              </Col>
+            </Row>
             </div>
           </Col>
-          
-          
-          <Col md ={6}>
+          <Col md ={8}>
             <Container className="right-container">
-              <Card>
+              <h1> About </h1>
+              <Card className="car">
                 <Card.Body>
                   <p>
-                  I am currently an  Computer Science and Engineering Grad. and I spend time building personal projects to improve my skills Right now I am focusing on Backend Development and Fullstack development, but I also have interests in Game Development and Machine Learning. Contact me at achoudhury2@scu.edu
+                    {about}
                   </p>
                 </Card.Body>
               </Card>
 
               <div id="experience">
+                <h1> Experience </h1>
                 {experiences.map(job => 
-                  <Card mb ={3}>
-                    <Row g={0} className = "flex" noGutters>
-                    <Card.Body>
-                      <Card.Title>{job.title}</Card.Title>
-                      <Card.Subtitle className="text-muted">{job.date.start} -- {job.date.end}</Card.Subtitle>
-                      <Card.Text>{job.description}</Card.Text>
-                    </Card.Body>
+                  <Card className="car" mb ={3}>
+                    <Row g={0} noGutters>
+                      <Card.Body>
+                        <Card.Title>{job.title}</Card.Title>
+                        <Card.Subtitle className="text-muted">{job.date.start} -- {job.date.end}</Card.Subtitle>
+                        <Card.Text>{job.description}</Card.Text>
+                      </Card.Body>
                     </Row>
                   </Card>
                 )}     
               </div>
 
+
               <div id="projects">
+                <h1>Projects</h1>
                 {projects.slice(0,7).map(project => 
-                  <Card mb ={3}> 
-                    <Row g={0} className = "flex" noGutters>
+                  <Card className="car" mb ={3}> 
+                    <Row g={0}>
                       <Col md = {4} >
                         <Image fluid src={project.image} />
                       </Col>
@@ -126,22 +119,23 @@ function Home() {
                         <Card.Body>
                           <Card.Title>{project.name}</Card.Title>
                           <Card.Subtitle className="text-muted">{project.date}</Card.Subtitle>
+                          
                           <Card.Text>
                             {project.description}
                           </Card.Text>
+
                           {project.links.Devpost ? (<Card.Link href ={project.links.Devpost}> Devpost </Card.Link>):<></>}
                           {project.links.Github ? (<Card.Link href ={project.links.Github}> Github </Card.Link>):<></>}
                           {project.links.Live ? (<Card.Link href ={project.links.Live}> Live </Card.Link>):<></>}
+
                         </Card.Body>
                       </Col>
                     </Row>
                   </Card>
-                )}     
-
+                )}
               </div>
             </Container>
           </Col>
-        </Row>
       </Row>
     </div>
   );
