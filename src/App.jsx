d023719975ca3ce6927c9c749d49133428f4e9f6
Arnav Choudhury <66573183+8coolguy@users.noreply.github.com>
2025-05-03 23:51:07 -0700

    [ADD] virtulization the shader by pause/play with debouncing

--- src/App.jsx
+++ src/App.jsx
@@ -56,11 +56,23 @@ function Shader({width, height, code, author, onError, onCompile}){
     "desynchronized": false
   }
   const sandbox = useRef(null);
+  const box = useRef(null);
+  const [scrolly, setScroll] = useState(0);
   const isGlsl2 = (newCode) =>{
     if(newCode.length == 0) return false;
     const version_dec = newCode.split("\n")[0];
     if(version_dec == "#version 300 es") return true;
     return false;
+  }
+
+  const isBoundingBox = (element, scroll) =>{
+    const rect = element.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    )
   }
   useEffect(() => {
     if(canvas.current && !sandbox.current){
@@ -74,6 +86,11 @@ function Shader({width, height, code, author, onError, onCompile}){
   useEffect(() => {
     handleChange(code);
   }, [code])
+  useEffect(() => {
+    handleScroll(scrolly);
+    window.addEventListener('scroll', () => setScroll(window.scrollY));
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [scrolly])
   function compile(newCode){
     const instance = sandbox.current;
     if(instance){
@@ -89,9 +106,22 @@ function Shader({width, height, code, author, onError, onCompile}){
     }, 700),
     []
   )
+  const handleScroll = useCallback(
+    debounce((newScrollY) => { 
+      const instance = sandbox.current;
+      if(instance){
+        if (isBoundingBox(box.current, newScrollY)){
+          instance.play();
+        }else{
+          instance.pause();
+        }
+      }
+    }, 300),
+    []
+  )
   
   return (
-    <div>
+    <div ref={box}>
       <canvas ref={canvas} id="canvas" height={height} width={width}></canvas>
       <p className="text-right">{author}</p>
     </div>
