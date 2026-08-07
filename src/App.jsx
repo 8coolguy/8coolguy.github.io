e55f61fcebbf49449afa7b06890347d7fc34e168
8coolguy <arnavchoudhury20@mittymonarch.com>
2026-08-07 00:42:02 -0700

    [CHG] revert gallery description, add book of shaders link to throw shader page

--- src/App.jsx
+++ src/App.jsx
@@ -402,6 +402,7 @@ function Thrower(){
           <div className="rounded-xl">
             <h1 className="text-4xl md:text-7xl text-center"> Throw Shader </h1>
             <Navigation/>
+            <p className="text-center text-gray-500 mt-6 mb-8">Learn how to write shaders with <a href="https://thebookofshaders.com" className="underline" target="_blank" rel="noopener noreferrer">the book of shaders</a>.</p>
             <form onSubmit={handleSubmit}>
               <div className="w-full mb-4" style={{aspectRatio: compactMode ? "2 / 1" : "1 / 1"}}>
                 <Shader width={700} height={700} code={code} author="" wrapClassName="w-full h-full" className="w-full h-full block" pauseOnHidden={false} onError={handleError} onCompile={() => setVisible(false)}/>
@@ -453,7 +454,7 @@ function Gallery() {
           <div className="rounded-xl">
             <h1 className="text-4xl md:text-7xl text-center"> Gallery </h1>
             <Navigation/>
-            <p className="text-center text-gray-500 mt-10 mb-12">Code to compute the color of every pixel. <a href="https://thebookofshaders.com" className="underline" target="_blank" rel="noopener noreferrer">Learn more</a> or <a href="/throwshader" className="underline">try it here.</a></p>
+            <p className="text-center text-gray-500 mt-6 mb-8">Code to compute the color of every pixel. <a href="/throwshader" className="underline">Try it here.</a></p>
             <div className="grid md:grid-cols-2 grid-cols-1 gap-8">
             {shaders.filter((element)=>{return element.code.length > 0 && !errored[element.id]}).map((element)=>{
               const code = JSON.parse(element.code);
