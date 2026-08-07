db11797ac2a907ee3584fb94e87d21299cbb3b44
8coolguy <arnavchoudhury20@mittymonarch.com>
2026-08-07 00:39:57 -0700

    [CHG] add Book of Shaders link, increase gallery spacing

--- src/App.jsx
+++ src/App.jsx
@@ -453,7 +453,7 @@ function Gallery() {
           <div className="rounded-xl">
             <h1 className="text-4xl md:text-7xl text-center"> Gallery </h1>
             <Navigation/>
-            <p className="text-center text-gray-500 mt-6 mb-8">Code to compute the color of every pixel. <a href="/throwshader" className="underline">Try it here.</a></p>
+            <p className="text-center text-gray-500 mt-10 mb-12">Code to compute the color of every pixel. <a href="https://thebookofshaders.com" className="underline" target="_blank" rel="noopener noreferrer">Learn more</a> or <a href="/throwshader" className="underline">try it here.</a></p>
             <div className="grid md:grid-cols-2 grid-cols-1 gap-8">
             {shaders.filter((element)=>{return element.code.length > 0 && !errored[element.id]}).map((element)=>{
               const code = JSON.parse(element.code);
